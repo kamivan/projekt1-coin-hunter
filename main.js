bf5cb45d3e2197322c453bcf505e0241ee3b39a0
Ivana <kamivan@email.cz>
2022-03-17 21:11:22 +0100

    fce init

--- main.js
+++ main.js
@@ -21,43 +21,55 @@ let figure = document.querySelector('#panacek');
 
 // nadefinujeme globální proměnné
 // ty jsou pak použitelné kdekoliv v programu
-let panacek, panacekX, panacekY, panacekSirka, panacekVyska;
+let figurine, panacekX, panacekY, panacekSirka, panacekVyska;
 let mince, mineX, minceY, minceSirka, minceVyska;
+let score, numberCoins;
+let soundCoin, soundWin;
 
 // tato funkce se spustí při načtení stránky
 // tj. ve chvíli, kdy je načtené komplet HTML, CSS a všechny obrázky a zvuky
-function priNacteniStranky() {
+function init() {
 
 	// do globálních proměnných si uložíme odkaz na objekty panáčka a mince,
 	// abychom je nemuseli při každém použití znovu na stránce hledat pomocí document.querySelector
+	figurine = document.querySelector('#panacek');
+	coin = document.querySelector('#mince');
+	score = document.querySelector('#score');
+	soundCoin = document.querySelector('#zvukmince');
+	soundWin = document.querySelector('#zvukfanfara');
 
+	console.log("init");
+	console.log(panacekX);
+
+	numberCoins = 0;
 
 	// zjistíme šířku a výšku panáčka
-
+	panacekSirka = figurine.width; 
+	panacekVyska = figure.height;
 
 	// a umístíme panáčka do středu okna
 
 
 	// umístíme panáčka na startovní pozici
-
+	placeFigurine();
 
 	// zjistíme šířku a výšku mince
 
 
 	// a vygenerujeme první minci na náhodné pozici
-
+	newCoin();
 
 }
 
 // funkce, která umístí panáčka na jeho souřadnice
 // tj. nastaví jeho style.left a style.top na hodnoty panacekX, panacekY
-function umistiPanacka() {
+function placeFigurine() {
 	// musíme to napsat :)
 }
 
 // funkce pro nahodné vygenerování nové pozice mince
 // a umístění mince na tyto souřadnice
-function novaMince() {
+function newCoin() {
 	// musíme to napsat :)
 }
 
@@ -92,4 +104,4 @@ function otestujKolizi() {
 	// musíme to napsat :)
 	if (!( panacekX + panacekSirka < minceX || minceX + minceSirka < panacekX || panacekY + panacekVyska < minceY || minceY + minceVyska < panacekY)) 
 		// panacek a mince se prekryvaji
-	}
+}
